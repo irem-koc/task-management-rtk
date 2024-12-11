052da77137191456c14371531773941accdef598
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-11 23:57:58 +0300

    Refactor fetchTodos function to use async/await for response.json()

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 export const fetchTodos = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-  const result = response.json();
-  return result.map((task) => ({
+  const result = await response.json();
+  return result.map((task: Task) => ({
     id: task.id,
     title: task.title,
     description: "",
