ea6553d1f9e4c277b3607978a554b1bc19162df1
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-11 23:57:50 +0300

    Add delete functionality to TaskList component

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,31 +1,28 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../features/store"; // Import types
-import { fetchTodo } from "../features/taskSlice";
+import { deleteTask, fetchTodo } from "../features/taskSlice";
+import EditTaskModal from "./EditTaskModal";
 
-type Props = {};
-
-const TaskList = (props: Props) => {
+const TaskList = () => {
   const tasks = useSelector((state: RootState) => state.tasks.tasks);
   const loading = useSelector((state: RootState) => state.tasks.loading);
   const dispatch = useDispatch<AppDispatch>();
-
   useEffect(() => {
     dispatch(fetchTodo());
   }, [dispatch]);
-
+  const handleDelete = (id: string) => {
+    dispatch(deleteTask(id));
+  };
   return (
     <ul>
       {loading ? (
         <li>Loading...</li>
       ) : (
-        <div className="mx-auto w-[1000px] p-5 border rounded-md bg-white">
-          <h1 className="text-indigo-700 font-bold text-3xl text-center">
-            Task Management App
-          </h1>
-          <p className="text-md text-start">Tasks</p>
+        <div>
+          <p className="text-md text-start mt-2 ">Tasks</p>
           <div className="gap-4 flex flex-col justify-between items-center">
-            {tasks.map((task) => (
+            {tasks.map((task: Task) => (
               <li
                 className="w-full rounded-md flex justify-between items-center bg-gray-50 p-5 "
                 key={task?.id}
@@ -34,13 +31,22 @@ const TaskList = (props: Props) => {
                   <span className="font-bold text-wrap whitespace-nowrap">
                     {task.title}
                   </span>
+                  <span className="italic text-gray-400">
+                    {task.description}
+                  </span>
                   <span>
-                    Status: <span className="italic">{task.status}</span>
+                    Status:{" "}
+                    <span className="italic underline">{task.status}</span>
                   </span>
                 </span>
                 <span className="flex gap-3 items-center text-white">
-                  <button className="rounded-md p-3 bg-blue-500">Edit</button>
-                  <button className="rounded-md p-3 bg-red-500">Delete</button>
+                  <EditTaskModal editedTask={task} />
+                  <button
+                    onClick={() => handleDelete(task.id)}
+                    className="rounded-md p-3 bg-red-500"
+                  >
+                    Delete
+                  </button>
                 </span>
               </li>
             ))}
