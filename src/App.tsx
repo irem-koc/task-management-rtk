a43a123bd42cdfba10abe693a9b23f49258796d6
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-11 23:46:57 +0300

    Add task management functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import AddTask from "./components/AddTask";
 import TaskList from "./components/TaskList";
 
 function App() {
   return (
-    <div className="bg-gray-100 pt-10">
-      <TaskList />
+    <div className="h-screen bg-gray-100 pt-10">
+      <div className="mx-auto w-[1000px] p-5 border rounded-md bg-white">
+        <h1 className="text-indigo-700 font-bold text-3xl text-center">
+          Task Management App
+        </h1>
+        <AddTask />
+        <TaskList />
+      </div>
     </div>
   );
 }
